b9602a7aa48a27934bad72ef20c3a4a93bd818da
anaximeno <anaximenobrito@gmail.com>
2023-02-12 22:03:30 -0100

    feat: add argument to change the number of images when searching

--- datasetter.py
+++ datasetter.py
@@ -7,21 +7,25 @@ import argparse
 import pandas as pd
 
 sys.path.append('./helpers')
-from ai_utilities import image_download
+from ai_utils import image_download
 
 
 DATASET_PATH = './data/dataset'
 PESTS_CSV_METADATA_PATH = './data/pestes e pragas - cabo verde.csv'
+DEFAULT_NUMBER_OF_IMAGES_PER_CLASS = 1000
 
 
-def download_images(keyworks: list[str], output_dir: str) -> None:
+def download_images(keyworks: list[str], imgs_per_class: int, output_dir: str) -> None:
     pass
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('datasetter')
+
     parser.add_argument('--out-dir', '--out_dir', type=str, required=False,
                         help='Determine the output dir for the downloaded images', default=DATASET_PATH)
+    parser.add_argument('-n', type=int, required=False,
+                        help='Number of images to search', default=DEFAULT_NUMBER_OF_IMAGES_PER_CLASS)
 
     args = parser.parse_args()
 
